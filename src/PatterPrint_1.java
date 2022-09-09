266eea7b2f6bcdd74ce9cd04cb62708643dafade
Sourabh <sourabhkosta1@gmail.com>
2022-09-09 16:54:51 +0530

    FirstCommit

---
+++ src/PatterPrint_1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PatterPrint_1
+{
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+
+
+
+
+    }
+}
