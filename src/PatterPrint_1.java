e4338b66fa0c370c32aa7f5c9193f80aa24064f7
Sourabh <sourabhkosta1@gmail.com>
2022-09-10 11:34:14 +0530

    Pattern class

--- src/PatterPrint_1.java
+++ src/PatterPrint_1.java
@@ -6,9 +6,15 @@ public class PatterPrint_1
     {
         Scanner s = new Scanner(System.in);
         int a = s.nextInt();
-
-
-
+        System.out.println("Series to be find "+a);
+        for(int i=1;i<a;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
 
     }
 }
